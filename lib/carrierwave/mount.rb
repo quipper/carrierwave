16fa642a6e09d1eaa81140bb4bd9d51ef3bcbf00
Tomo Nakano <tomo@quipack.com>
2011-02-16 18:42:23 +0000

    temporary fix for mongoid bug #https://github.com/mongoid/mongoid/issues/173

--- lib/carrierwave/mount.rb
+++ lib/carrierwave/mount.rb
@@ -291,6 +291,8 @@ module CarrierWave
         uploader.cache!(new_file)
         @integrity_error = nil
         @processing_error = nil
+        write_identifier
+        store!
       rescue CarrierWave::IntegrityError => e
         @integrity_error = e
         raise e unless option(:ignore_integrity_errors)
